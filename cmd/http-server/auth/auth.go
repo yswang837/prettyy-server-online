a4c469e105a54a024ebe0f7c95f870e3d0b0aa10
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-08-21 15:23:55 +0800

    将所有接口的验签方式改为： pin+按key sort过后的key1=value1&key2=value2

--- cmd/http-server/auth/auth.go
+++ cmd/http-server/auth/auth.go
@@ -1,93 +1,122 @@
 package auth
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"fmt"
 	"github.com/emirpasic/gods/sets/hashset"
 	"github.com/gin-gonic/gin"
+	"net/url"
 	"prettyy-server-online/cmd/http-server/conf"
 	ginConsulRegister "prettyy-server-online/custom-pkg/xzf-gin-consul/register"
+	"prettyy-server-online/utils/tool"
+	"sort"
+	"strings"
 )
 
 var (
 	authConfig = map[string]struct {
-		signAttr    []string
 		allowCaller *hashset.Set
 	}{
-		conf.URLIdentifyCodeByEmail: {signAttr: []string{"email"}, allowCaller: hashset.New("test")},
-		conf.URLIdentifyCode:        {signAttr: []string{""}, allowCaller: hashset.New("test")},
-		conf.URLRegisterLogin:       {signAttr: []string{"email", "password", "identity_code"}, allowCaller: hashset.New("test")},
-		conf.URLCheckPassword:       {signAttr: []string{"email"}, allowCaller: hashset.New("test")},
-		conf.URLLoginOut:            {signAttr: []string{""}, allowCaller: hashset.New("test")},
-		conf.URLUpdateNickName:      {signAttr: []string{"email", "nick_name"}, allowCaller: hashset.New("test")},
-		conf.URLUpdateGender:        {signAttr: []string{"email", "gender"}, allowCaller: hashset.New("test")},
-		conf.URLUpdateSummary:       {signAttr: []string{"email", "summary"}, allowCaller: hashset.New("test")},
-		conf.URLUpdateProvinceCity:  {signAttr: []string{"email", "province", "city"}, allowCaller: hashset.New("test")},
-		conf.URLUpdateBirthday:      {signAttr: []string{"email"}, allowCaller: hashset.New("test")},
-		conf.URLUpdatePassword:      {signAttr: []string{"email", "password"}, allowCaller: hashset.New("test")},
-		conf.URLPublishArticle:      {signAttr: []string{"title", "summary", "uid"}, allowCaller: hashset.New("test")},
-		conf.URLGetArticleList:      {signAttr: []string{"page", "page_size"}, allowCaller: hashset.New("test")},
-		conf.URLGetArticleDetail:    {signAttr: []string{"page", "page_size"}, allowCaller: hashset.New("test")},
-		conf.URLFileUpload:          {signAttr: []string{""}, allowCaller: hashset.New("test")},
+		conf.URLIdentifyCodeByEmail: {allowCaller: hashset.New("test", "web")},
+		conf.URLIdentifyCode:        {allowCaller: hashset.New("test", "web")},
+		conf.URLRegisterLogin:       {allowCaller: hashset.New("test", "web")},
+		conf.URLCheckPassword:       {allowCaller: hashset.New("test", "web")},
+		conf.URLLoginOut:            {allowCaller: hashset.New("test", "web")},
+		conf.URLUpdateNickName:      {allowCaller: hashset.New("test", "web")},
+		conf.URLUpdateGender:        {allowCaller: hashset.New("test", "web")},
+		conf.URLUpdateSummary:       {allowCaller: hashset.New("test", "web")},
+		conf.URLUpdateProvinceCity:  {allowCaller: hashset.New("test", "web")},
+		conf.URLUpdateBirthday:      {allowCaller: hashset.New("test", "web")},
+		conf.URLUpdatePassword:      {allowCaller: hashset.New("test", "web")},
+		conf.URLPublishArticle:      {allowCaller: hashset.New("test", "web")},
+		conf.URLGetArticleList:      {allowCaller: hashset.New("test", "web")},
+		conf.URLGetArticleDetail:    {allowCaller: hashset.New("test", "web")},
+		conf.URLFileUpload:          {allowCaller: hashset.New("test", "web")},
 	}
 	callerPin = map[string]string{
 		"test": "098f6bcd4621d373cade4e832627b4f6", // 测试的调用方和pin，
+		"web":  "4d3abacc23255a0767c718afda30cc8c", // web浏览器的调用方和pin，
 	}
 )
 
 func Auth(ginCtx *gin.Context) {
 	ctx := ginConsulRegister.NewContext(ginCtx)
 	// 开发业务代码阶段，可以临时注释一下
-	//if !auth(ctx) {
-	//	ctx.Abort()
-	//	return
-	//}
+	if !auth(ctx) {
+		ctx.Abort()
+		return
+	}
 	ctx.Next()
 }
 
 func auth(ctx *ginConsulRegister.Context) bool {
 	// 检查url是否允许
-	url := ctx.Request.URL.Path
-	config, ok := authConfig[url]
+	urlStr := ctx.Request.URL.Path
+	config, ok := authConfig[urlStr]
 	if !ok {
-		ctx.JSON(200, &ginConsulRegister.Response{Code: 400001, Message: fmt.Sprintf("url %s is not allow", url)})
+		ctx.JSON(200, &ginConsulRegister.Response{Code: 400001, Message: fmt.Sprintf("url %s is not allow", urlStr)})
 		return false
 	}
-	var getParam = func(key string) string {
-		value, ok := ctx.GetQuery(key)
-		if !ok {
-			value = ctx.PostForm(key)
-		}
-		return value
-	}
 	// 检查caller是否允许
-	caller := getParam("caller")
+	caller := getParam(ctx, "caller")
 	if caller == "" {
 		ctx.JSON(200, &ginConsulRegister.Response{Code: 400002, Message: "caller is empty"})
 		return false
 	}
 	ctx.SetCaller(caller)
 	if config.allowCaller != nil && !config.allowCaller.Contains(caller) {
-		ctx.JSON(200, &ginConsulRegister.Response{Code: 400003, Message: fmt.Sprintf("caller: %s is forbidden for %s", caller, url)})
+		ctx.JSON(200, &ginConsulRegister.Response{Code: 400003, Message: fmt.Sprintf("caller: %s is forbidden for %s", caller, urlStr)})
 		return false
 	}
-
 	// 检查签名是否正确
-	var signSlice []string
-	for _, attr := range config.signAttr {
-		signSlice = append(signSlice, getParam(attr))
+	sign := getParam(ctx, "sign")
+	if sign == "" {
+		ctx.JSON(200, &ginConsulRegister.Response{Code: 400004, Message: "sign is empty"})
+		return false
 	}
-	signStr := ""
-	for _, value := range signSlice {
-		signStr += value
-	}
-	sign := getParam("sign")
-	b := md5.Sum([]byte(callerPin[caller] + signStr))
-	expectedSign := hex.EncodeToString(b[:])
+	signStr := getParam(ctx, "")
+	expectedSign := tool.ToMd5(callerPin[caller] + signStr)
 	if expectedSign != sign {
-		ctx.JSON(200, &ginConsulRegister.Response{Code: 400004, Message: fmt.Sprintf("signature error. expected %s, actuall %s", expectedSign, sign)})
+		ctx.JSON(200, &ginConsulRegister.Response{Code: 400005, Message: fmt.Sprintf("signature error. expected %s, actuall %s", expectedSign, sign)})
 		return false
 	}
 	return true
+}
+
+func getParam(ctx *ginConsulRegister.Context, key string) string {
+	// key如果不为空，则获取单个属性值，
+	// key如果为空则获取所有属性值，并且是排好序的值的字符串拼接，用于验签
+	switch ctx.Request.Method {
+	case "GET":
+		if key != "" {
+			value, _ := ctx.GetQuery(key)
+			return value
+		} else {
+			return sortParams(ctx.Request.URL.Query())
+		}
+	case "POST":
+		if key != "" {
+			return ctx.PostForm(key)
+		} else {
+			// 获取所有的参数
+			return sortParams(ctx.Request.PostForm)
+		}
+	}
+	return ""
+}
+
+func sortParams(m url.Values) string {
+	if len(m) == 0 {
+		return ""
+	}
+	var params []string
+	for k, v := range m {
+		if k == "sign" {
+			continue
+		}
+		if len(v) != 0 {
+			params = append(params, fmt.Sprintf("%s=%s", k, v[0]))
+		}
+	}
+	sort.Strings(params)
+	return strings.Join(params, "&")
 }
