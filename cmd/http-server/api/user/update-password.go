849af60357d7169d2e5aec2312523bf50e658dd8
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-10-23 10:13:58 +0800

    update-password,update-summary 添加日志和metrics

--- cmd/http-server/api/user/update-password.go
+++ cmd/http-server/api/user/update-password.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	ginConsulRegister "prettyy-server-online/custom-pkg/xzf-gin-consul/register"
 	"prettyy-server-online/services/user"
+	"prettyy-server-online/utils/metrics"
 )
 
 // updatePasswordParams 面向接口
@@ -16,31 +17,42 @@ type updatePasswordParams struct {
 // 4000300
 // 2000300
 func (s *Server) UpdatePassword(ctx *ginConsulRegister.Context) {
+	metrics.CommonCounter.Inc("update-password", "total")
 	p := &updatePasswordParams{}
 	if err := ctx.Bind(p); err != nil {
+		metrics.CommonCounter.Inc("update-password", "params-error")
+		ctx.SetError(err.Error())
 		ctx.JSON(http.StatusBadRequest, ginConsulRegister.Response{Code: 4000300, Message: "参数错误"})
 		return
 	}
+	ctx.SetUid(p.Uid).SetPassword(p.Password)
 	// 密码长度控制在6~20位
 	if len(p.Password) < 6 || len(p.Password) > 20 {
+		metrics.CommonCounter.Inc("update-password", "invalid-password")
 		ctx.JSON(http.StatusBadRequest, ginConsulRegister.Response{Code: 4000301, Message: "密码长度必须在6~20个字符"})
 		return
 	}
 	u, err := user.GetUser(p.Uid)
 	if err != nil {
+		metrics.CommonCounter.Inc("update-password", "get-user-error")
+		ctx.SetError(err.Error())
 		ctx.JSON(http.StatusBadRequest, ginConsulRegister.Response{Code: 4000302, Message: "获取用户信息失败"})
 		return
 	}
 	if u.Password != "" && u.Password == p.Password {
 		// 库中的密码不为空，且密码相同
+		metrics.CommonCounter.Inc("update-password", "same-password")
 		ctx.JSON(http.StatusBadRequest, ginConsulRegister.Response{Code: 4000303, Message: "密码未改变"})
 		return
 	}
 	// 要么是库中密码为空，要么是密码不同，均可以直接更新密码，binding required 已经保证了密码不为空
 	if err = user.UpdatePassword(p.Uid, p.Password); err != nil {
+		metrics.CommonCounter.Inc("update-password", "update-password-error")
+		ctx.SetError(err.Error())
 		ctx.JSON(http.StatusBadRequest, ginConsulRegister.Response{Code: 4000304, Message: "密码更新失败"})
 		return
 	}
+	metrics.CommonCounter.Inc("update-password", "succ")
 	ctx.JSON(http.StatusOK, ginConsulRegister.Response{Code: 2000300, Message: "密码更新成功"})
 	return
 }
--- cmd/http-server/api/user/update-summary.go
+++ cmd/http-server/api/user/update-summary.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	ginConsulRegister "prettyy-server-online/custom-pkg/xzf-gin-consul/register"
 	"prettyy-server-online/services/user"
+	"prettyy-server-online/utils/metrics"
 )
 
 // updateNickNameParams 面向接口
@@ -16,24 +17,34 @@ type updateSummaryParams struct {
 // 4000240
 // 2000240
 func (s *Server) UpdateSummary(ctx *ginConsulRegister.Context) {
+	metrics.CommonCounter.Inc("update-summary", "total")
 	p := &updateSummaryParams{}
 	if err := ctx.Bind(p); err != nil {
+		metrics.CommonCounter.Inc("update-summary", "params-error")
+		ctx.SetError(err.Error())
 		ctx.JSON(http.StatusBadRequest, ginConsulRegister.Response{Code: 4000240, Message: "参数错误"})
 		return
 	}
+	ctx.SetUid(p.Uid).SetSummary(p.Summary)
 	u, err := user.GetUser(p.Uid)
 	if err != nil {
+		metrics.CommonCounter.Inc("update-summary", "get-user-error")
+		ctx.SetError(err.Error())
 		ctx.JSON(http.StatusBadRequest, ginConsulRegister.Response{Code: 4000241, Message: "获取用户信息失败"})
 		return
 	}
 	if p.Summary == u.Summary {
+		metrics.CommonCounter.Inc("update-summary", "same-summary")
 		ctx.JSON(http.StatusBadRequest, ginConsulRegister.Response{Code: 4000242, Message: "个人简介未改变"})
 		return
 	}
 	if err = user.UpdateSummary(p.Uid, p.Summary); err != nil {
+		metrics.CommonCounter.Inc("update-summary", "update-summary-error")
+		ctx.SetError(err.Error())
 		ctx.JSON(http.StatusBadRequest, ginConsulRegister.Response{Code: 4000243, Message: "个人简介更新失败"})
 		return
 	}
+	metrics.CommonCounter.Inc("update-summary", "succ")
 	ctx.JSON(http.StatusOK, ginConsulRegister.Response{Code: 2000240, Message: "个人简介更新成功"})
 	return
 }
